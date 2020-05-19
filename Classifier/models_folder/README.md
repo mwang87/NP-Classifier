513cdc2e2ea7b5d5eb47b9d1925e6a3be9feb878
Mingxun Wang <mwang87@gmail.com>
2020-05-18 19:45:53 -0700

    adding actual reporting

--- Classifier/models_folder/README.md
+++ Classifier/models_folder/README.md
@@ -2,3 +2,6 @@
 
 What we need to do is convert keras hdf5 model to tensor flow model. 
 
+1. Unzip
+1. Convert from keras -> tf model
+1. Place model in appropriate folder also in versioned folder e.g. 0000001
--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from flask import Flask
 import json
 import pandas as pd
 import requests
+import numpy as np
 
 import sys
 sys.path.insert(0, "Classifier")
@@ -78,7 +79,32 @@ def handle_smiles(smiles_string):
 
 def classify_structure(smiles):
     fp = fingerprint_handler.calculate_fingerprint(smiles, 2)
-    return fp
+
+    query_dict = {}
+    query_dict["input_1"] = fp[0].tolist()[0]
+    query_dict["input_2"] = fp[1].tolist()[0]
+
+    fp_pred_url = "http://npclassifier-tf-server:8501/v1/models/superclassifier:predict"
+    payload = json.dumps({"instances": [ query_dict ]})
+
+    
+    headers = {"content-type": "application/json"}
+    json_response = requests.post(fp_pred_url, data=payload, headers=headers)
+    print(json_response.text)
+
+    classified_prediction = np.asarray(json.loads(json_response.text)['predictions'])
+    classification_indices = np.where(classified_prediction[0] >= 0.5)[0]
+
+    import pickle
+    with open('Classifier/dict/0512_char2idx_super.pkl','rb') as char2idx_super:
+        super_class = pickle.load(char2idx_super)
+    
+    all_classifications = []
+    for XXX in classification_indices:
+        classification = [name for name, age in super_class.items() if age == XXX][0]
+        all_classifications.append(classification)
+
+    return all_classifications
 
 if __name__ == "__main__":
     app.run_server(debug=True, port=5000, host="0.0.0.0")
