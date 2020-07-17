e1bcb265fc0e6d421d0f6fedf52fd31816cba49d
ChungmaruQ <51690359+ChungmaruQ@users.noreply.github.com>
2020-07-16 20:37:28 -0700

    Update fingerprint_handler.py

--- Classifier/fingerprint_handler.py
+++ Classifier/fingerprint_handler.py
@@ -13,31 +13,29 @@ from rdkit import DataStructs
 #(@ming if we want to use inchi as an input, inchi should be changed to SMILES and the SMILES should be standardized)
 def calculate_fingerprint(smiles, radi):
     binary = np.zeros((2048*(radi)), int)
-    formula = np.zeros((2048), int)
-
+    formula = np.zeros((2048),int)
     mol = Chem.MolFromSmiles(smiles)
-    #mol = Chem.MolFromSmiles(smiles, sanitize=False)
-    mol = Chem.AddHs(mol)
-    #mol = Chem.AddHs(mol, explicitOnly=True, addCoords=True, onlyOnAtoms=True)
     
+    mol = Chem.AddHs(mol)
     mol_bi = {}
     for r in range(radi+1):
         mol_fp = rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, radius=r, bitInfo=mol_bi, nBits = 2048)
         mol_bi_QC = []
         for i in mol_fp.GetOnBits():
-            idx = mol_bi[i][0][0]
-            radius_list = []
             num_ = len(mol_bi[i])
             for j in range(num_):
                 if mol_bi[i][j][1] == r:
                     mol_bi_QC.append(i)
                     break
 
+       
         if r == 0:
             for i in mol_bi_QC:
                 formula[i] = len([k for k in mol_bi[i] if k[1]==0])
         else:
             for i in mol_bi_QC:
                 binary[(2048*(r-1))+i] = 1
-
-    return formula.reshape(1,2048), binary.reshape(1,4096)
+    
+    formula[1652] = 0 # Single proton is removed to make normalization easy
+    
+    return formula.reshape(1,2048),binary.reshape(1,4096)
