e1b69589b00fabd5ddde4505d282b8e6568cf419
Mingxun Wang <mwang87@gmail.com>
2020-07-20 16:59:48 -0700

    adding speed test

--- test-integration/test.py
+++ test-integration/test.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import urllib.parse
 import requests
+from tqdm import tqdm
+
 
 SERVER_URL = "http://dorresteintesthub.ucsd.edu:6541"
 #SERVER_URL = "http://mingwangbeta.ucsd.edu:6541"
@@ -12,7 +14,16 @@ def test():
         smiles = str(entry["smiles"])
         if len(smiles) > 5:
             request_url = "{}/classify?smiles={}".format(SERVER_URL, urllib.parse.quote(smiles))
-            print(request_url)
             r = requests.get(request_url)
             classification = r.json()
-            print(classification)
+
+def test_speed():
+    df = pd.read_csv("test.tsv", sep="\t")
+
+    for i in tqdm(range(1000)):
+        for entry in df.to_dict(orient="records"):
+            smiles = str(entry["smiles"])
+            if len(smiles) > 5:
+                request_url = "{}/classify?smiles={}".format(SERVER_URL, urllib.parse.quote(smiles))
+                r = requests.get(request_url)
+                classification = r.json()
