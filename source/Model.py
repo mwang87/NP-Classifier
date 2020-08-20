99fa781058ee4660e69384471164d0b12c507a4f
ChungmaruQ <51690359+ChungmaruQ@users.noreply.github.com>
2020-08-20 11:08:10 -0700

    Update Model.py

--- source/Model.py
+++ source/Model.py
@@ -11,27 +11,18 @@ def top_k_categorical_accuracy(y_true, y_pred):
 def model_build(num): # num = number of categories
     input_f = layers.Input(shape=(2048,))
     input_b = layers.Input(shape=(4096,))
-    Xb = layers.Dense(4096, activation = 'relu')(input_b)
-    Xb = layers.BatchNormalization()(Xb)
-    Xb = layers.Dense(2048, activation = 'relu')(Xb)
-    Xb = layers.BatchNormalization()(Xb)
-    Xb = layers.Dense(1024, activation = 'relu')(Xb)
+    input_fp = layers.Concatenate()([input_f,input_b])
     
-    Xf = layers.Dense(2048, activation = 'relu')(input_f)
-    Xf = layers.BatchNormalization()(Xf)
-    Xf = layers.Dense(1024, activation = 'relu')(Xf)
-    Xf = layers.BatchNormalization()(Xf)
-    Xf = layers.Dense(512, activation = 'relu')(Xf)
-    
-    X = layers.Concatenate()([Xb,Xf])
+    X = layers.Dense(6144, activation = 'relu')(input_fp)
+    X = layers.BatchNormalization()(X)
+    X = layers.Dense(3072, activation = 'relu')(X)
+    X = layers.BatchNormalization()(X)
+    X = layers.Dense(1536, activation = 'relu')(X)
     X = layers.BatchNormalization()(X)
     X = layers.Dense(1536, activation = 'relu')(X)
     X = layers.Dropout(0.2)(X)
-    
-    output1 = layers.Dense(num, activation = 'sigmoid', name = 'Class')(X)
-
-    model = keras.Model(inputs = [input_f,input_b], outputs = output1)
-    
+    output = layers.Dense(num, activation = 'sigmoid')(X)
+    model = keras.Model(inputs = [input_f,input_b], outputs = output)
     model.compile(optimizer=keras.optimizers.Adam(lr=0.00001),loss=['binary_crossentropy'],metrics=['cosine_proximity',top_k_categorical_accuracy])
     return model
 
