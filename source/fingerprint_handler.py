d9c3c53480e39c30a228382da885439923e30601
ChungmaruQ <51690359+ChungmaruQ@users.noreply.github.com>
2020-08-20 11:09:08 -0700

    Update fingerprint_handler.py

--- source/fingerprint_handler.py
+++ source/fingerprint_handler.py
@@ -33,7 +33,7 @@ def calculate_fingerprint(smiles, radi):
                 formula[i] = len([k for k in mol_bi[i] if k[1]==0])
         else:
             for i in mol_bi_QC:
-                binary[(2048*(r-1))+i] = 1
+                binary[(2048*(r-1))+i] = len([k for k in mol_bi[i] if k[1]==r])
     
     formula[1652] = 0 # Single proton is removed to make normalization easy
     
