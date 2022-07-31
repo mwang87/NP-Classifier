ba5b4287372cff946d0c2c4e6032339098b5f930
Mingxun Wang <mwang87@gmail.com>
2022-07-30 21:30:56 -0700

    increasing parallelism

--- run_server.sh
+++ run_server.sh
@@ -3,4 +3,4 @@
 source activate rdkit
 
 #python ./app.py
-gunicorn -w 2 --threads=6 --worker-class=gthread -b 0.0.0.0:5000 --timeout 3600 app:server --access-logfile /app/logs/access.log --max-requests 1000
+gunicorn -w 8 --threads=6 --worker-class=gthread -b 0.0.0.0:5000 --timeout 3600 app:server --access-logfile /app/logs/access.log --max-requests 1000
