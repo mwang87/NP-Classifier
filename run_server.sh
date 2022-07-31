bfed336624a91d863dfb629adf2e6bb87ba7239b
Mingxun Wang <mwang87@gmail.com>
2022-07-30 21:56:06 -0700

    updaitng restart interval

--- run_server.sh
+++ run_server.sh
@@ -3,4 +3,4 @@
 source activate rdkit
 
 #python ./app.py
-gunicorn -w 4 --threads=6 --worker-class=gthread -b 0.0.0.0:5000 --timeout 20 app:server --access-logfile /app/logs/access.log --max-requests 1000
+gunicorn -w 4 --threads=6 --worker-class=gthread -b 0.0.0.0:5000 --timeout 20 app:server --access-logfile /app/logs/access.log --max-requests 250
